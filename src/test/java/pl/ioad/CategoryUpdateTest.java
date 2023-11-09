f14becec8d61cfff08bd38622e48d362ccbc2ab9
matkrasinski <mat.krasinski@o2.pl>
2023-11-09 21:07:50 +0100

    PUT Category methods tests

--- src/test/java/pl/ioad/CategoryUpdateTest.java
+++ src/test/java/pl/ioad/CategoryUpdateTest.java
@@ -6,78 +6,37 @@ import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
 import static io.restassured.RestAssured.given;
-import static java.net.HttpURLConnection.HTTP_NO_CONTENT;
-import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED;
+import static java.net.HttpURLConnection.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.lessThan;
 import static pl.ioad.utils.CategoryCreator.createCategory;
 import static pl.ioad.utils.Credentials.ADMIN;
+import static pl.ioad.utils.UniqueNameGenerator.generateUniqueCategoryName;
 
-public class CategoryDeleteTest {
+public class CategoryUpdateTest {
 
-    private final static String CATEGORY_ENDPOINT = "/categories";
-    private final List<String> categoriesIds = new ArrayList<>();
-
-    private final int categoriesSize = getClass().getDeclaredMethods().length - 3;
+    private String categoryId;
+    private static final String CATEGORY_ENDPOINT = "/categories";
     @Before
     public void setUp() {
         RestAssured.baseURI = "https://api.practicesoftwaretesting.com";
-        IntStream.range(0, categoriesSize).forEach(i -> categoriesIds.add(createCategory()));
+        categoryId = createCategory();
     }
 
 
     @Test
-    public void testDeleteCategoryUnauthorized() {
+    public void testUpdateCategoryUnauthorized() {
         Response response = given()
                 .contentType(ContentType.JSON)
-                .when()
-                .delete(CATEGORY_ENDPOINT + "/" + categoriesIds.get(0));
-
-        response.then()
-                .assertThat()
-                .statusCode(HTTP_UNAUTHORIZED)
-                .header("Content-Type", containsString(ContentType.JSON.toString()))
-                .header("Server", not(emptyString()));
-
-        assertThat(response.time(), lessThan(5000L)); // Temporary 5000 ms
-    }
-
-    @Test
-    public void testDeleteCategoryAuthorized() {
-        var accessToken = AuthHelper.getAccessToken(ADMIN);
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + accessToken)
-                .when()
-                .delete(CATEGORY_ENDPOINT + "/" + categoriesIds.get(0));
-
-        response.then()
-                .assertThat()
-                .statusCode(HTTP_NO_CONTENT)
-                .header("Server", not(emptyString()));
-
-        assertThat(response.time(), lessThan(5000L)); // Temporary 5000 ms
-        categoriesIds.remove(0);
-    }
-
-    @Test
-    public void testDeleteCategoryIncorrectCategoryId() {
-        var accessToken = AuthHelper.getAccessToken(ADMIN);
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + accessToken)
                 .when()
                 .delete(CATEGORY_ENDPOINT + "/-1");
 
         response.then()
                 .assertThat()
-                .statusCode(422)
-                .body("id[0]", equalTo("The selected id is invalid."))
+                .statusCode(HTTP_UNAUTHORIZED)
+                .body("message", equalTo("Unauthorized"))
                 .header("Content-Type", containsString(ContentType.JSON.toString()))
                 .header("Server", not(emptyString()));
 
@@ -85,41 +44,95 @@ public class CategoryDeleteTest {
     }
 
     @Test
-    public void testDeleteCategoryWithQueryParams() {
+    public void testUpdateCategoryDoesntExist() {
         var accessToken = AuthHelper.getAccessToken(ADMIN);
+
         Response response = given()
-                .queryParam("id", categoriesIds.get(0))
                 .contentType(ContentType.JSON)
                 .header("Authorization", "Bearer " + accessToken)
                 .when()
-                .delete(CATEGORY_ENDPOINT + "/" + categoriesIds.get(0));
+                .put(CATEGORY_ENDPOINT + "/-1");
 
         response.then()
                 .assertThat()
-                .statusCode(HTTP_NO_CONTENT)
+                .statusCode(HTTP_OK)
+                .body("success", equalTo(false))
+                .header("Content-Type", containsString(ContentType.JSON.toString()))
                 .header("Server", not(emptyString()));
 
         assertThat(response.time(), lessThan(5000L)); // Temporary 5000 ms
-        categoriesIds.remove(0);
     }
 
     @Test
-    public void testDeleteCategoryWithRequestBody() {
+    public void testUpdateCategoryDataChanged() {
         var accessToken = AuthHelper.getAccessToken(ADMIN);
+
+        String categoryName = generateUniqueCategoryName();
+        String requestBody = "{\"name\": \"" + categoryName + "\", \"slug\": \"" + categoryName + "_slug\"}";
+
         Response response = given()
-                .body("{id: " + categoriesIds.get(0) + "}")
                 .contentType(ContentType.JSON)
                 .header("Authorization", "Bearer " + accessToken)
+                .body(requestBody)
                 .when()
-                .delete(CATEGORY_ENDPOINT + "/" + categoriesIds.get(0));
+                .put(CATEGORY_ENDPOINT + "/" + categoryId);
+
 
         response.then()
                 .assertThat()
-                .statusCode(HTTP_NO_CONTENT)
+                .statusCode(HTTP_OK)
+                .body("success", equalTo(true))
+                .header("Content-Type", containsString(ContentType.JSON.toString()))
                 .header("Server", not(emptyString()));
 
         assertThat(response.time(), lessThan(5000L)); // Temporary 5000 ms
-        categoriesIds.remove(0);
     }
 
+    @Test
+    public void testUpdateCategoryChangedToEmpty() {
+        var accessToken = AuthHelper.getAccessToken(ADMIN);
+
+        String requestBody = "{\"name\": \"" + "" + "\", \"slug\": \"" + "\"}";
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + accessToken)
+                .body(requestBody)
+                .when()
+                .put(CATEGORY_ENDPOINT + "/" + categoryId);
+
+        response.then()
+                .assertThat()
+                .statusCode(422)
+                .body("name[0]", equalTo("The name field must be a string."))
+                .body("slug[0]", equalTo("The slug field must be a string."))
+                .header("Content-Type", containsString(ContentType.JSON.toString()))
+                .header("Server", not(emptyString()));
+
+        assertThat(response.time(), lessThan(5000L)); // Temporary 5000 ms
+    }
+
+    @Test
+    public void testUpdateCategoryChangedOnlyOneField() {
+        var accessToken = AuthHelper.getAccessToken(ADMIN);
+        String categoryName = generateUniqueCategoryName();
+
+        String requestBody = "{\"name\": \"" + categoryName +"}";
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + accessToken)
+                .body(requestBody)
+                .when()
+                .put(CATEGORY_ENDPOINT + "/" + categoryId);
+
+        response.then()
+                .assertThat()
+                .statusCode(HTTP_OK)
+                .body("success", equalTo(true))
+                .header("Content-Type", containsString(ContentType.JSON.toString()))
+                .header("Server", not(emptyString()));
+
+        assertThat(response.time(), lessThan(5000L)); // Temporary 5000 ms
+    }
 }
